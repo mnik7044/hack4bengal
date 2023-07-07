1d6ae326b3a1f5978adf5a412777c6e4262dc80e
Ekam-Bitt <ekambitt@gmail.com>
2023-07-07 13:31:54 +0530

    Fixed redirection to Profile page after Sign in/up

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,18 +1,22 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from "react-router-dom";
 
 export function useAuthStatus() {
-    const[loggedIn, setLoggedIn] = useState(false)
-    const [checkingStatus, setCheckingStatus] = useState(true)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true)
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user) => {
-            if(user){
-                setLoggedIn(true)
-            }
-            setCheckingStatus(false)
-        })
-    }, [])
+  useEffect(() => {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true)
+        navigate("../Profile")
+      }
+      setCheckingStatus(false)
+    })
+  }, [navigate]) // Add `navigate` to the dependency array
+
   return { loggedIn, checkingStatus };
 }
