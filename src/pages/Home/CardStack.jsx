2f9a72708ceab3554401a4856379d61a42673941
Rajdip Sinh <rajdips834@gmail.com>
2023-07-08 17:28:05 +0530

    Minor centering commit

--- src/pages/Home/CardStack.jsx
+++ src/pages/Home/CardStack.jsx
@@ -1,14 +1,14 @@
 import Card from "./Card";
 export default function CardStack() {
   return (
-    <div className="flex">
+    <div className="flex p-4 justify-items-center">
       <Card title="Anxiety" cause1="stress" className="left-[50px]">
         {}
       </Card>
-      <Card className="left-[350px]"></Card>
-      <Card className="left-[650px]"></Card>
-      <Card className="left-[950px]"></Card>
-      <Card className="left-[1250px]"></Card>
+      <Card className=""></Card>
+      <Card className=""></Card>
+      <Card className=""></Card>
+      <Card className=""></Card>
     </div>
   );
 }
