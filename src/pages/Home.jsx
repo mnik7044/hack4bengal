7d1845dc3a9df809de3cb1c3f90cfb303824f45b
Rajdip Sinh <rajdips834@gmail.com>
2023-07-10 01:08:55 +0530

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,10 @@ import background from "../assets/back.png";
 import doctors from "../assets/doctors.png";
 import CardStack from "./Home/CardStack";
 import image from "../assets/image1.png";
+import image1 from "../assets/image4.png";
+import image2 from "../assets/image2.png";
+import image3 from "../assets/image3.png";
+import ArticleCard from "./Home/ArticleCard";
 export default function Home() {
   // const [screenSize, setScreenSize] = useState(getCurrentDimension());
 
@@ -55,12 +59,12 @@ export default function Home() {
             <label htmlFor="simple-search" className="sr-only">
               Search
             </label>
-            <div className="relative p-10 ">
+            <div className="relative p-10 left-4 ">
               <div className="absolute inset-y-0 left-0 flex items-center pl-1 pointer-events-none"></div>
               <input
                 type="text"
                 id="simple-search"
-                className=" bg-slate-100 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-50 focus:border-violet-50 block w-[150vh] pl-4 p-7  dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-[82vw] p-5 pl-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-slate-100 focus:ring-violet-50 focus:border-violet-50 dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Condition/Doctor/Procedure      |      City/State/Pin-Code   "
                 required=""
               />
@@ -68,7 +72,7 @@ export default function Home() {
 
             <button
               type="submit"
-              className="p-2.5 ml-2 left-14 text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
+              className="relative p-5 ml-2 text-white bg-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-800"
             >
               <svg
                 className="w-4 h-4"
@@ -91,8 +95,12 @@ export default function Home() {
           </form>
         </div>
       </div>
-
-      <img src={image} alt="babayaga"></img>
+      <div className="relative justify-between inline-flex flex-row ml-[4.5rem] w-[80%]">
+        <ArticleCard pic={image}></ArticleCard>
+        <ArticleCard pic={image1}></ArticleCard>
+        <ArticleCard pic={image2}></ArticleCard>
+        <ArticleCard pic={image3}></ArticleCard>
+      </div>
     </div>
   );
 }
--- src/pages/Home/Searchbar.jsx
+++ src/pages/Home/Searchbar.jsx
@@ -10,7 +10,7 @@ export default function Searchbar() {
           <input
             type="text"
             id="simple-search"
-            className=" bg-slate-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-50 focus:border-violet-50 block w-[80vh] pl-4 p-3  dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className=" bg-slate-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-50 focus:border-violet-50 block w-[140vh]   dark:bg-slate-50 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Condition/Doctor/Procedure      |      City/State/Pin-Code   "
             required=""
           />
