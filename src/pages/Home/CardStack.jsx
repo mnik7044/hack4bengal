92c68dd236735e99c0faa805d439585b0eaf1f14
Rajdip Sinh <rajdips834@gmail.com>
2023-07-10 04:28:11 +0530

    Minor changes 2

--- src/pages/Home/CardStack.jsx
+++ src/pages/Home/CardStack.jsx
@@ -1,9 +1,7 @@
 import Card from "./Card";
 export default function CardStack(props) {
   return (
-    <div className="relative flex justify-between ">
-      <Card className=""></Card>
-      <Card className=""></Card>
+    <div className="relative flex justify-between left-56 ">
       <Card className=""></Card>
       <Card className=""></Card>
       <Card className=""></Card>
