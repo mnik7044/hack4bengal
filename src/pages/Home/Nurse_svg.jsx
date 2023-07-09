c96671efd0340d11bab26eb149e726f4dabbff68
Rajdip Sinh <rajdips834@gmail.com>
2023-07-10 04:30:21 +0530

    bleh

--- src/pages/Home/Nurse_svg.jsx
+++ src/pages/Home/Nurse_svg.jsx
@@ -2,7 +2,7 @@ import { ReactComponent as Nurse } from "../../assets/Landingpage.svg";
 
 export default function Nurse_svg() {
   return (
-    <div className="pt-2 pl-60">
+    <div className="w-[400px] pt-2 pl-60">
       <Nurse></Nurse>
     </div>
   );
