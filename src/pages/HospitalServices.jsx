1b85b06761b9ccd386495d1a51888d5c38b1a1f9
Rajdip Sinh <rajdips834@gmail.com>
2023-07-10 12:17:04 +0530

    Hospital Services

--- src/pages/HospitalServices.jsx
+++ src/pages/HospitalServices.jsx
@@ -1,3 +1,9 @@
 export default function HospitalServices() {
-  return <></>;
+  return (
+    <div>
+      <h1 className="text-4xl w-[60%] font-Lato">
+        Look for availability of Hospital beds/OTs/Ambulances across your city
+      </h1>
+    </div>
+  );
 }
--- src/pages/MentalHealth.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function MentalHealth() {
-  return (
-    <div>MentalHealth</div>
-  )
-}
